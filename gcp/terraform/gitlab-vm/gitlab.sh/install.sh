bf994b0152c574253df8588186b74537af97016c
PedrOps <it.pedrops@gmail.com>
2022-06-13 23:28:50 -0300

    Update install.sh

--- gcp/terraform/gitlab-vm/gitlab.sh/install.sh
+++ gcp/terraform/gitlab-vm/gitlab.sh/install.sh
@@ -4,3 +4,6 @@ sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
 sudo apt-get install -y wget telnet
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 sudo EXTERNAL_URL="https://gitlab.example.com" apt-get install gitlab-ee
+sudo apt-get purge appamor -y
+sudo hostnamectl set-hostname gitlab01
+sudo timedatectl set-timezone America/Sao_Paulo
