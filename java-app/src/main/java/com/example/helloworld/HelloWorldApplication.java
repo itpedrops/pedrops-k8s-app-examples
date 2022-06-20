34e2ec46f93d8d3a4edc80ee3e035ec0a6d29de0
PedrOps <it.pedrops@gmail.com>
2022-06-19 23:18:59 -0300

    Update HelloWorldApplication.java

--- java-app/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ java-app/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.example.HelloWorldApplication;
+package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
